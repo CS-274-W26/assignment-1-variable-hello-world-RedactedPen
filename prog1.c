07ffe59fb3e5e7833f267ed8db92feed4af36e4c
Skye Zickuhr <john@zickuhr.com>
2026-01-09 10:19:34 -0800

    Added more extensive comments

--- prog1.c
+++ prog1.c
@@ -6,19 +6,18 @@
 
 
 int main() {
-	// The main function is the entrypoint of the program. When the program
-	// is executed, it will automatically start here.
-	
-	// TODO Complete the program
-	//
+	//Generate the random number
 	srand(time(NULL));
 	int myrand = rand() % 11;
-
+	
+	//Verify the number is within the expected bounds
+	//Unecessary, but error checking never hurt.
 	if(myrand < 0 || myrand > 10){
 		printf("Invalid random number generated\n");
 		exit(1);
 	}
-
+	
+	//Print the first statement
 	if(myrand <= 4){
 		printf("Eat more beef, kick less cats\n");
 	}else if( myrand <= 9){
@@ -26,8 +25,10 @@ int main() {
 	}else{
 		printf("Larn is the best rougelike\n");
 	}
-
+	
+	//Print the second statement
 	printf("The random number was %d\n", myrand);
-
+	
+	//Again, technically unecessary
 	exit(0);
 }
