a352a5164c240399b9740e4ee26aae8795f12dc3
Skye Zickuhr <john@zickuhr.com>
2026-01-09 10:14:53 -0800

    Finished assignment

--- prog1.c
+++ prog1.c
@@ -1,4 +1,6 @@
 #include <stdio.h> // Necessary for printf()
+#include <stdlib.h>
+#include <time.h>
 
 // TODO Other includes
 
@@ -8,4 +10,24 @@ int main() {
 	// is executed, it will automatically start here.
 	
 	// TODO Complete the program
+	//
+	srand(time(NULL));
+	int myrand = rand() % 11;
+
+	if(myrand < 0 || myrand > 10){
+		printf("Invalid random number generated\n");
+		exit(1);
+	}
+
+	if(myrand <= 4){
+		printf("Eat more beef, kick less cats\n");
+	}else if( myrand <= 9){
+		printf("FRODO LIVES\n");
+	}else{
+		printf("Larn is the best rougelike\n");
+	}
+
+	printf("The random number was %d\n", myrand);
+
+	exit(0);
 }
